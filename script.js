627c126ca9e6d2f9c6145613bde14de4a7ea8e6f
Marat123480 <78693401+Marat123480@users.noreply.github.com>
2021-09-08 20:55:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    document.getElementById("percent-100").style.width="100%";
+    document.getElementById("lg-percent-50").style.width="50%";
+    document.getElementById("percent-30").style.width="30%";
+	document.getElementById("percent-90").style.width="90%";
+    document.getElementById("percent-80").style.width="80%";
+    document.getElementById("percent-75").style.width="75%";
+    document.getElementById("percent-50").style.width="50%";
+}
+
+
+
+
+
+
